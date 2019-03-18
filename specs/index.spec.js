8b8bef0830f041fed5c7b2932f97fb511cdd2aa3
Martin Haynes <martin.haynes@creative-assembly.com>
2019-03-18 15:06:27 +0000

    fixes style issues

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,162 +1,184 @@
 
-const {expect} = require('chai');
-const {event, ctx, callback} = require('./mocks');
+const {expect} = require("chai");
+const {event, ctx, callback} = require("./mocks");
 
-const LambdaController = require('../index');
+const LambdaController = require("..");
 
 const mockController = () => new LambdaController(event.valid, ctx.noop, callback.noop);
 
-describe("LambdaController", function () {
-    
-    describe('Constractor', function () {
-        it("should construct the class and load the event", function() {
-            expect(mockController().event).to.deep.equal(event.valid);
-        });
-    });
+describe("LambdaController", () => {
+	describe("Constractor", () => {
+		it("should construct the class and load the event", () => {
+			expect(mockController().event).to.deep.equal(event.valid);
+		});
+	});
 
-    describe('Properties', function () {
-        it("should return the query string as an object of param/value" , function () {
-            const controller = mockController();
-            expect(controller.query).to.be.an('object').and.deep.equal(event.valid.queryStringParameters);
-        });
+	describe("Properties", () => {
+		it("should return the query string as an object of param/value", () => {
+			const controller = mockController();
+			expect(controller.query).to.be.an("object").and.deep.equal(event.valid.queryStringParameters);
+		});
 
-        it("should return the path", function () {
-            const controller = mockController();
-            expect(controller.path).to.be.a("string").and.equal(event.valid.path);
-        });
+		it("should return the path", () => {
+			const controller = mockController();
+			expect(controller.path).to.be.a("string").and.equal(event.valid.path);
+		});
 
-        it("should return the http method", function () {
-            const controller = mockController();
-            expect(controller.method).to.be.a("string").and.equal(event.valid.httpMethod);
-        });
+		it("should return the http method", () => {
+			const controller = mockController();
+			expect(controller.method).to.be.a("string").and.equal(event.valid.httpMethod);
+		});
 
-        it("should return the http headers as an object of header/value", function () {
-            const controller = mockController();
-            expect(controller.headers).to.be.an('object').and.equal(event.valid.headers);
-        });
+		it("should return the http headers as an object of header/value", () => {
+			const controller = mockController();
+			expect(controller.headers).to.be.an("object").and.equal(event.valid.headers);
+		});
 
-        it("Should return the path params as an object of param/value", function () {
-            const controller = mockController();
-            expect(controller.params).to.be.an('object').and.equal(event.valid.pathParameters);
-        });
-    });
+		it("Should return the path params as an object of param/value", () => {
+			const controller = mockController();
+			expect(controller.params).to.be.an("object").and.equal(event.valid.pathParameters);
+		});
+	});
 
-    describe('Methods', function () {
-        it("#addHeader should add a response header", function () {
-            const controller = mockController();
-            const headerName = "foo";
-            const headerValue = "bar";
-            controller.addHeader(headerName, headerValue);
-            expect(controller.response.headers).to.be.an("object")
-                .and.deep.equal({
-                    [headerName]: headerValue
-                });
-        });
+	describe("Methods", () => {
+		it("#addHeader should add a response header", () => {
+			const controller = mockController();
+			const headerName = "foo";
+			const headerValue = "bar";
+			controller.addHeader(headerName, headerValue);
+			expect(controller.response.headers).to.be.an("object")
+				.and.deep.equal({
+					[headerName]: headerValue
+				});
+		});
 
-        it("#statusCode should change the response status code", function () {
-            const controller = mockController();
-            const statusCode = 301;
-            controller.status(statusCode);
-            expect(controller.response.statusCode).to.be.a("number").and.equal(statusCode);
-        });
+		it("#statusCode should change the response status code", () => {
+			const controller = mockController();
+			const statusCode = 301;
+			controller.status(statusCode);
+			expect(controller.response.statusCode).to.be.a("number").and.equal(statusCode);
+		});
 
-        it("#body should change the response body", function () {
-            const controller = mockController();
-            const body = "hello world!";
-            controller.body(body);
-            expect(controller.response.body).to.be.a("string").and.equal(body);
-        });
+		it("#body should change the response body", () => {
+			const controller = mockController();
+			const body = "hello world!";
+			controller.body(body);
+			expect(controller.response.body).to.be.a("string").and.equal(body);
+		});
 
-        it("#type should up the header Content-Type to the MIME Type", function () {
-            const controller = mockController();
-            const mime = "application/javascript";
-            controller.type(mime);
-            expect(controller.response.headers["Content-Type"]).to.be.a("string").and.equal(mime);
-        });
+		it("#type should up the header Content-Type to the MIME Type", () => {
+			const controller = mockController();
+			const mime = "application/javascript";
+			controller.type(mime);
+			expect(controller.response.headers["Content-Type"]).to.be.a("string").and.equal(mime);
+		});
 
-        it("#json should set the body to stringify JSON, and set the MIME Type", function () {
-            const controller = mockController();
-            const obj = {foo: "bar"};
-            const mime = "application/json";
+		it("#json should set the body to stringify JSON, and set the MIME Type", () => {
+			const controller = mockController();
+			const obj = {foo: "bar"};
+			const mime = "application/json";
 
-            controller.json(obj);
+			controller.json(obj);
 
-            expect(controller.response.headers["Content-Type"]).to.be.a("string").and.equal(mime);
-            expect(controller.response.body).to.be.a('string').and.equal(JSON.stringify(obj));
-        });
-    });
+			expect(controller.response.headers["Content-Type"]).to.be.a("string").and.equal(mime);
+			expect(controller.response.body).to.be.a("string").and.equal(JSON.stringify(obj));
+		});
+	});
 
-    describe("Send", function () {
-        it("should send the response by calling the callback", function (done) {
-            const body = "hello world!";
-            const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
-                expect(res.body).to.be.a('string').and.equal(body);
-                done();
-            });
+	describe("Send", () => {
+		it("should send the response by calling the callback", (done) => {
+			const body = "hello world!";
+			const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
+				if (err) {
+					throw new Error("An Error occurred");
+				}
 
-            controller.body(body);
-            controller.send();
-        });
+				expect(res.body).to.be.a("string").and.equal(body);
+				done();
+			});
 
-        it("should send an object as serialized json with the correct MIME type", function (done) {
-            const body = {foo: "bar"};
-            const mime = "application/json";
-            const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
-                expect(res.body).to.be.a('string').and.equal(JSON.stringify(body));
-                expect(res.headers["Content-Type"]).to.be.a("string").and.equal(mime);
-                done();
-            });
+			controller.body(body);
+			controller.send();
+		});
 
-            controller.sendJson(body);
-        });
+		it("should send an object as serialized json with the correct MIME type", (done) => {
+			const body = {foo: "bar"};
+			const mime = "application/json";
+			const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
+				if (err) {
+					throw new Error("An Error occurred");
+				}
 
-        it("should send an HTTP Error response with the correct status code and message", function (done) {
-            const code = 403;
-            const body = "You are not allowed to access this resource";
-            const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
-                expect(res.body).to.be.a('string').and.equal(body);
-                expect(res.statusCode).to.be.a('number').and.equal(code);
-                done();
-            })
+				expect(res.body).to.be.a("string").and.equal(JSON.stringify(body));
+				expect(res.headers["Content-Type"]).to.be.a("string").and.equal(mime);
+				done();
+			});
 
-            controller.error(code, body);
-        });
+			controller.sendJson(body);
+		});
 
-        it("Should send an HTTP error with a status code of 404 and a message", function (done) {
-            const code = 404;
-            const body = "Resource was not found";
-            const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
-                expect(res.body).to.be.a('string').and.equal(body);
-                expect(res.statusCode).to.be.a('number').and.equal(code);
-                done();
-            })
+		it("should send an HTTP Error response with the correct status code and message", (done) => {
+			const code = 403;
+			const body = "You are not allowed to access this resource";
+			const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
+				if (err) {
+					throw new Error("An Error occurred");
+				}
 
-            controller.notFound();
-        });
+				expect(res.body).to.be.a("string").and.equal(body);
+				expect(res.statusCode).to.be.a("number").and.equal(code);
+				done();
+			});
 
-        it("Should send an HTTP error with a status code of 500 and a message", function (done) {
-            const code = 500;
-            const body = "Resource unavailable";
-            const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
-                expect(res.body).to.be.a('string').and.equal(body);
-                expect(res.statusCode).to.be.a('number').and.equal(code);
-                done();
-            })
+			controller.error(code, body);
+		});
 
-            controller.serverError();
-        });
+		it("Should send an HTTP error with a status code of 404 and a message", (done) => {
+			const code = 404;
+			const body = "Resource was not found";
+			const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
+				if (err) {
+					throw new Error("An Error occurred");
+				}
 
-        it("Should send an HTTP error with a status code of 400 and a message", function (done) {
-            const code = 400;
-            const body = "Bad request to resource";
-            const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
-                expect(res.body).to.be.a('string').and.equal(body);
-                expect(res.statusCode).to.be.a('number').and.equal(code);
-                done();
-            })
+				expect(res.body).to.be.a("string").and.equal(body);
+				expect(res.statusCode).to.be.a("number").and.equal(code);
+				done();
+			});
 
-            controller.badRequest();
-        });
-    });
+			controller.notFound();
+		});
 
+		it("Should send an HTTP error with a status code of 500 and a message", (done) => {
+			const code = 500;
+			const body = "Resource unavailable";
+			const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
+				if (err) {
+					throw new Error("An Error occurred");
+				}
+
+				expect(res.body).to.be.a("string").and.equal(body);
+				expect(res.statusCode).to.be.a("number").and.equal(code);
+				done();
+			});
+
+			controller.serverError();
+		});
+
+		it("Should send an HTTP error with a status code of 400 and a message", (done) => {
+			const code = 400;
+			const body = "Bad request to resource";
+			const controller = new LambdaController(event.valid, ctx.noop, (err, res) => {
+				if (err) {
+					throw new Error("An Error occurred");
+				}
+
+				expect(res.body).to.be.a("string").and.equal(body);
+				expect(res.statusCode).to.be.a("number").and.equal(code);
+				done();
+			});
+
+			controller.badRequest();
+		});
+	});
 });
